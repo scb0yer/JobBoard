5f97c6cd8f422afbac23bd909b4fd36841afe0ef
Sophie Boyer <sboyer@MacBook-Air-de-Sophie-2.local>
2023-12-14 21:17:02 +0100

    2nd commit with interpolation class

--- src/components/Job.jsx
+++ src/components/Job.jsx
@@ -1,6 +1,6 @@
 const Job = (props) => {
   return (
-    <div className={props.color}>
+    <div className={`${props.color} box`}>
       <div className="title">{props.title}</div>
       <div className="details">
         {props.contractType} - {props.country} - {props.city}
